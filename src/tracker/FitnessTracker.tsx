d5387bc2a56471a735cdc662b467af6d7c310eb0
Amir Afzali <amir@afzali.ca>
2022-01-14 04:34:13 -0500

    Finish fitness tracker and add new material UI package

--- src/tracker/FitnessTracker.tsx
+++ src/tracker/FitnessTracker.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { collection, doc, setDoc } from 'firebase/firestore';
+import { collection, deleteDoc, doc, setDoc } from 'firebase/firestore';
 import { db } from "../firebase/firebase";
 import { useLocation } from "react-router-dom";
 import { getDocs } from 'firebase/firestore';
@@ -8,6 +8,7 @@ import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import Divider from "@mui/material/Divider";
 import AddIcon from '@mui/icons-material/Add';
+import Edit from "@mui/icons-material/Edit";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
@@ -15,19 +16,23 @@ import Accordion from "@mui/material/Accordion";
 import Typography from "@mui/material/Typography";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
 import TextField from "@mui/material/TextField";
 import DialogActions from "@mui/material/DialogActions";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import './tracker.css'
+import IconButton from "@mui/material/IconButton";
+import { Autocomplete } from "@mui/material";
+import { MenuItem } from "@mui/material";
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
 
 enum ExerciseGroup {
     CHEST = "Chest",
     BACK = "Back",
     SHOULDERS = "Shoulders",
     LEGS = "Legs",
-    GLUTES = "Glutes",
     ABS = "Abs",
     BICEPS = "Biceps",
     TRICEPS = "Triceps",
@@ -45,7 +50,7 @@ enum TimeUnit {
     HOURS = "HOURS"
 }
 
-const COMMON_EXERCISES =  {
+const COMMON_EXERCISES: {[key: string]: string[]} =  {
     [ExerciseGroup.CHEST]: [
         "Flat Benchpress - Barbell", "Incline Benchpress - Barbell",
         "Flat Benchpress - Dumbbell", "Incline Benchpress - Dumbbell",
@@ -115,10 +120,6 @@ const makeFitnessLog = (
     }
 }
 
-const updateFitnessLog = async (log: Log, username: string) => {
-    const logDoc = doc(db, `/fitness/all/${username}/${log.creationDate}`)
-    setDoc(logDoc, log)
-}
 
 export const TrackMenu = () => {
     return (<div>
@@ -133,10 +134,7 @@ export const FitnessTracker = () => {
     const location: {[key: string]: any} = useLocation();
     const [username, setUsername] = useState<string>(location.state);
     const [addOpen, setAddOpen] = useState(false);
-  
-    const handleClickOpen = () => {
-        setAddOpen(true);
-    };
+    const [editLog, setEditLog] = useState<Log | null>(null);
   
     const handleClose = () => {
         setAddOpen(false);
@@ -150,39 +148,50 @@ export const FitnessTracker = () => {
             const fitnessMap: {[key: number]: Log} = {}
             docData.forEach(e => fitnessMap[e.creationDate] = e)
             setFitnessLogs(fitnessMap)
-            console.log('x')
         }
         fetchExisting();
     }, [username])
 
-    const testAddRoutine = () => {
-        const ex = makeFitnessLog(
-            ExerciseGroup.BACK,
-            "Dumbbell Row",
-            30,
-            WeightUnit.KG,
-            8,
-            4,
-            0,
-            TimeUnit.SECONDS
-        )
-
-        updateFitnessLog(ex, username)
-        setFitnessLogs({...fitnessLogs, [ex.creationDate]: ex});
+    const updateFitnessLog = async (log: Log) => {
+        const logDoc = doc(db, `/fitness/all/${username}/${log.creationDate}`)
+        await setDoc(logDoc, log)
+        setFitnessLogs({...fitnessLogs, [log.creationDate]: log})
     }
 
+    const deleteFitnessLog = async (log: Log) => {
+        const logDoc = doc(db, `/fitness/all/${username}/${log.creationDate}`)
+        await deleteDoc(logDoc);
+        let logsCopy = Object.assign({}, fitnessLogs)
+        delete logsCopy[log.creationDate]
+        setFitnessLogs(logsCopy)
+    }
+
+    const updateEditLog = (log: Log | null) => {
+        setEditLog(log);
+        setAddOpen(true);
+    }
 
     return (
         <div className="tracker-container">
+            <LogPrompt 
+                handleClose={handleClose}
+                open={addOpen}
+                updateFitnessLog={updateFitnessLog}
+                deleteFitnessLog={deleteFitnessLog}
+                existingLog={editLog}
+            />
             <h1 style={{textAlign: 'center'}}>Personal Fitness Tracker</h1> 
             <div className="logging-container">
-                <FitnessGroups fitnessLogs={fitnessLogs} />
+                <FitnessGroups
+                    fitnessLogs={fitnessLogs}
+                    updateEditLog={updateEditLog} 
+                />
             </div>
             <div className="log-btn-row">
                 <Fab
                     color="primary" 
                     aria-label="add"
-                    onClick={() => testAddRoutine()}
+                    onClick={() => {setEditLog(null); setAddOpen(true)}}
                     className="new-log-button"
                 >
                     <AddIcon />
@@ -193,15 +202,17 @@ export const FitnessTracker = () => {
 }
 
 interface RowProps {
-    row: Log
+    row: Log,
+    updateEditLog: Function
 }
 
 interface FitnessGroupsProps {
-    fitnessLogs: {[key: number]: Log}
+    fitnessLogs: {[key: number]: Log},
+    updateEditLog: Function
 }
 
 export const FitnessGroups = (props: FitnessGroupsProps) => {
-    const {fitnessLogs} = props;
+    const {fitnessLogs, updateEditLog} = props;
 
     const uniqueGroups: {[key: string]: Log[]} = {};
     Object.values(fitnessLogs).forEach(e => {
@@ -229,7 +240,11 @@ export const FitnessGroups = (props: FitnessGroupsProps) => {
                                     return (
                                         <CollapsableSegment key={exercise} title={exercise}>
                                             {rows2.map(e => (
-                                                <FitnessRow row={e} key={e.creationDate}></FitnessRow>
+                                                <FitnessRow 
+                                                    row={e}
+                                                    key={e.creationDate}
+                                                    updateEditLog={updateEditLog}
+                                                ></FitnessRow>
                                             ))}
                                         </CollapsableSegment>
                                     )
@@ -245,13 +260,18 @@ export const FitnessGroups = (props: FitnessGroupsProps) => {
 }
 
 export const FitnessRow = (props: RowProps) => {
-    const {row} = props;
+    const {row, updateEditLog} = props;
 
     const formattedDate = new Date(row.eventDate).toLocaleDateString();
     const formattedDesc = `Weight: ${row.weight} ${row.weightUnit} |
                             ${row.sets} Sets x ${row.reps} Reps | Date: ${formattedDate}`;
     return (<div>
-        <ListItem>
+        <ListItem
+        secondaryAction={
+            <IconButton onClick={() => updateEditLog(row)} edge="end" aria-label="delete">
+              <Edit />
+            </IconButton>
+          }>
             <ListItemText primary={row.exercise} secondary={formattedDesc} />
         </ListItem>
         <Divider/>
@@ -275,32 +295,170 @@ export const CollapsableSegment = (props: any) => {
     )
 }
 
-export const NewLogPrompt = (props: any) => {
-    const {open, handleClose} = props;  
+export const LogPrompt = (props: any) => {
+    const {open, handleClose, existingLog, updateFitnessLog, deleteFitnessLog} = props;
+    const [formFields, setFormFields] = useState<Log>({} as Log);
+    console.log('x',existingLog)
+    let title = "Add New Fitness Log";
+    let updateButtonText = "Create";
+
+    useEffect(() => {
+        if(!open) {
+            setTimeout(() => setFormFields({} as Log), 1000)
+        } else {
+            setFormFields({} as Log)
+        }
+        
+    }, [open])
+    
+    if(!existingLog && !formFields.group) {
+        const date = new Date().getTime();
+        setFormFields({
+            creationDate: date,
+            eventDate: date,
+            group: ExerciseGroup.CHEST,
+            exercise: COMMON_EXERCISES[ExerciseGroup.CHEST][0],
+            weight: 135,
+            weightUnit: WeightUnit.LB,
+            reps: 8,
+            sets: 4,
+            duration: 0,
+            timeUnit: TimeUnit.MINUTES
+        });
+        console.log('t')
+    } else if(existingLog && !formFields.group) {
+        setFormFields(existingLog)
+        console.log('y')
+    }
+
+    if(existingLog) {
+        title = "Edit Fitness Log"
+        updateButtonText = "Update";
+    }
+
+    const onChange = (v: any, key: string) => {
+        setFormFields({...formFields, [key]: v})
+        console.log(v)
+    }
+
+    const updateProcess = () => {
+        console.log('t')
+        updateFitnessLog(formFields);
+        handleClose()
+    }
+    
+    console.log(title)
     return (
-      <div>
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>Subscribe</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              To subscribe to this website, please enter your email address here. We
-              will send updates occasionally.
-            </DialogContentText>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="Email Address"
-              type="email"
-              fullWidth
-              variant="standard"
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button onClick={handleClose}>Subscribe</Button>
-          </DialogActions>
+        <div>
+        <Dialog open={open} onClose={handleClose} fullWidth>
+            <DialogTitle>{title}</DialogTitle>
+            <DialogContent>
+            <div className="input-row">
+                <Autocomplete
+                    disablePortal
+                    options={Object.values(ExerciseGroup)}
+                    sx={{ width: 300, marginRight: 4 }}
+                    value={formFields.group}
+                    renderInput={(params) => <TextField {...params} variant="standard" />}
+                    onChange={(e,v) => {
+                        console.log('x', v)
+                        if(v) {
+                            setFormFields({
+                                ...formFields,
+                                group: v,
+                                exercise: COMMON_EXERCISES[v][0]
+                            })
+                        }
+                        
+                    }}
+                />
+                <Autocomplete
+                    disablePortal
+                    options={COMMON_EXERCISES[formFields.group as string]}
+                    sx={{ width: 300 }}
+                    value={formFields.exercise}
+                    renderInput={(params) => <TextField {...params} variant="standard" />}
+                    onChange={(e,v) => onChange(v,'exercise')}
+                />
+            </div>
+            <br></br>
+            <div className="input-row">
+                <div style={{display: 'flex', flexDirection: 'row', justifyContent: "center"}}>
+                    <TextField
+                        id="name"
+                        type="number"
+                        inputProps={{min: 0}}
+                        fullWidth
+                        variant="standard"
+                        value={formFields.weight}
+                        style={{width: "40%", marginRight: 10}}
+                        onChange={(e) => onChange(e.target.value, 'weight')}
+                        label="Weight"
+                    />
+                    <TextField
+                        id="standard-select-currency"
+                        select
+                        value="KG"
+                        onChange={(e) => onChange(e.target.value, 'weightUnit')}
+                        variant="standard"
+                        label="Unit"
+                        >
+                        {Object.values(WeightUnit).map((option) => (
+                            <MenuItem key={option} value={option}>
+                                {option}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                </div>
+                <div style={{display: 'flex', flexDirection: 'row', justifyContent: "center"}}>
+                <TextField
+                    id="name"
+                    type="number"
+                    inputProps={{min: 0}}
+                    fullWidth
+                    variant="standard"
+                    value={formFields.sets}
+                    style={{width: "20%"}}
+                    onChange={(e) => onChange(e.target.value, 'sets')}
+                    label="Sets"
+                />
+
+                <TextField
+                    id="name"
+                    type="number"
+                    inputProps={{min: 0}}
+                    fullWidth
+                    variant="standard"
+                    value={formFields.reps}
+                    style={{width: "20%", marginLeft: 10}}
+                    onChange={(e) => onChange(e.target.value, 'reps')}
+                    label="Reps"
+                />
+                </div>
+            </div>
+            <br></br>
+            <div style={{marginTop: 10, display: 'flex', flexDirection: 'row', justifyContent: "center"}}>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                    value={new Date(formFields.eventDate)}
+                    onChange={(newValue) => {
+                        if(newValue)
+                            onChange(newValue.getTime(), 'eventDate')
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                    label="Workout Date"
+                />
+            </LocalizationProvider>
+            </div>
+            </DialogContent>
+            <DialogActions>
+            {
+                existingLog && <Button color="error" onClick={() => {handleClose(); deleteFitnessLog(existingLog)}}>Delete</Button>
+            }
+            <Button style={{color: 'gray'}} onClick={handleClose}>Cancel</Button>
+            <Button color="primary" onClick={() => updateProcess()}>{updateButtonText}</Button>
+            </DialogActions>
         </Dialog>
-      </div>
+        </div>
     );
-  }
+}
