619072d570cfea1819c79d3fd0d65e9a41b9b0a7
Graeme Woods <graemewoods202@gmail.com>
2022-01-18 16:54:20 -0500

    Recommendations page work

--- src/recommendations/recommendations.tsx
+++ src/recommendations/recommendations.tsx
@@ -1,165 +1,128 @@
-import { ReactChild, ReactFragment, ReactPortal, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
 import { collection, deleteDoc, doc, query, getDocs, where, QuerySnapshot } from 'firebase/firestore';
 import { db } from "../firebase/firebase";
+import { Container, Row } from "react-bootstrap";
+import Table from 'react-bootstrap/Table'
 import { useNavigate, useLocation } from "react-router-dom";
-import { get_profile_data } from "../profile/profile_functions"
-
+import { firebaseAuth } from "../firebase/firebase";
+import { getWorkoutTypes } from "../search/Search";
 
 
 
 export const Recommendations = () => {
-    const location: {[key: string]: any} = useLocation();
-    const [username, setUsername] = useState<string>(location.state);
-    const navigate = useNavigate(); // intents between pages
-    let dataSet: any[] = [];
-    let usernames: any[] = [];
-    let schools: String[] = [];
-    let exercises: any[] = [];
+    const location:{ [key: string]: any } = useLocation();
+    const navigate = useNavigate();
+    const [username, setUsername]= useState<string>(location.state);
+
+    // const [userRecs, setUserRecs] = useState<{[key: number]: Log}>([]);
+    const [userRecs, setUserRecs] = useState<TableRow[]>([]);
+    // const [userRecs, setUserRecs] = useState<Log | null>(null);
+
+    // check if session active
+    firebaseAuth.onAuthStateChanged((user:any) => {
+        if (user){
+            console.log("session active");
+        }
+        else{
+        // session expired
+        navigate("/");
+        }
+    })
+    
+    let SCHOOLS: { [key: string]: string } =  {
+        mcmaster: "McMaster",
+        uwaterloo: "University of Waterloo",
+        uoft: "University of Toronto"
+    }
+    
+    let GYMS: { [key: string]: string } = {
+        'mcmaster_dbac': "McMaster DBAC",
+        'uwaterloo_gym2': "UWaterloo Gym 2",
+        'uoft_gym1': "U of T Gym 1",
+        'uwaterloo_gym1': "UWaterloo Gym "
+    }
+    
+    let EXERCISES: { [key: string]: string } = {
+        back: "Back",
+        biceps: "Biceps",
+        cardio: "Cardio",
+        chest: "Chest",
+        core: "Core",
+        shoulders: "Shoulders",
+        triceps: "Triceps",
+    }
+    interface TableRow {
+        'username': String,
+        'gyms': String,
+        'school': String,
+        'exercises': String[]
+    }
+
     useEffect(() => {
-        async function fetchExisting() {
+        (async () => {
             const profileCollection = collection(db,"Profile");
             const q = query(profileCollection, where("School", "==", "mcmaster"))
-            const querySnapshot = await getDocs(q);
-            querySnapshot.forEach((doc) => {
-                dataSet.push(doc);
-                //console.log(doc.data());
-                /*
-                usernames.push(doc.id);
-                schools.push(doc.get("School"));
-                exercises.push(doc.get("Workout_types"));
-                console.log(usernames[0]);
-                console.log(schools[0]);
-                console.log(exercises[0]); */
-                console.log(doc.id);
-                console.log(doc.get("School"));
-                console.log(doc.get("Workout_types"));
-                console.log(doc.get("Gyms"));
-
-            });
+            const docs = await getDocs(q);                
             
-                  
-        }
-        fetchExisting();
-    }, [username])
+            let reccomendations: TableRow[] = [];
+            docs.forEach(doc => {            
+                reccomendations.push({
+                    'username': doc.id,
+                    'school': SCHOOLS[`${doc.get("School")}`],
+                    'gyms': GYMS[`${doc.get("Gyms")}`],
+                    'exercises': doc.get("Workout_types").map((workout: string) => EXERCISES[`${workout}`],)
+                });
+            });            
+            console.log(reccomendations);                  
+            setUserRecs(reccomendations);                  
+        })();        
+    }, [])
 
     return (
-        <div>
-            <table id='rTable'>
-                <thead>
-                    <tr>
-                        <th>
-                            Name
-                        </th>
-                        <th>
-                            School
-                        </th>
-                        <th>
-                            Gym
-                        </th>
-                        <th>
-                            Exercises
-                        </th>
-                        <th>
-                            Appointment
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {dataSet.map((doc) => (
+        <Container>
+            <Row>
+                <h2 style={{marginBottom: 10}}>Recomendations</h2>
+                <Table responsive bordered>
+                    <thead>
                         <tr>
-                            <td>{doc.id}</td>
-                            <td>{doc.get("School")}</td>
-                            <td>{doc.get("Gyms")}</td>
-                            <td>{doc.get("Workout_types")}</td>
-                            <td>Appointment Button Here</td>
+                            <th>Name</th>
+                            <th>School</th>
+                            <th>Gym</th>
+                            <th>Exercises</th>
+                            <th>Appointment</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    )
-
-
-
-
-
-
-}
-
-
-
-
-/*
-export const FitnessTracker = () => {
-    let [groupTime, setGroupTime] = useState(false);
-    let [fitnessLogs, setFitnessLogs] = useState<{[key: number]: Log}>([]);
-    const location: {[key: string]: any} = useLocation();
-    const [username, setUsername] = useState<string>(location.state);
-    const [addOpen, setAddOpen] = useState(false);
-    const [editLog, setEditLog] = useState<Log | null>(null);
-  
-    const handleClose = () => {
-        setAddOpen(false);
-    };
-
-    useEffect(() => {
-        async function fetchExisting() {
-            const allLogs = collection(db, `/fitness/all/${username}`)
-            const docs = await getDocs(allLogs);
-            const docData = docs.docs.map(e => e.data() as FitnessLog);
-            const fitnessMap: {[key: number]: Log} = {}
-            docData.forEach(e => fitnessMap[e.creationDate] = e)
-            setFitnessLogs(fitnessMap)
-        }
-        fetchExisting();
-    }, [username])
-
-    const updateFitnessLog = async (log: Log) => {
-        const logDoc = doc(db, `/fitness/all/${username}/${log.creationDate}`)
-        await setDoc(logDoc, log)
-        setFitnessLogs({...fitnessLogs, [log.creationDate]: log})
-    }
-
-    const deleteFitnessLog = async (log: Log) => {
-        const logDoc = doc(db, `/fitness/all/${username}/${log.creationDate}`)
-        await deleteDoc(logDoc);
-        let logsCopy = Object.assign({}, fitnessLogs)
-        delete logsCopy[log.creationDate]
-        setFitnessLogs(logsCopy)
-    }
-
-    const updateEditLog = (log: Log | null) => {
-        setEditLog(log);
-        setAddOpen(true);
-    }
-
-    return (
-        <div className="tracker-container">
-            <LogPrompt 
-                handleClose={handleClose}
-                open={addOpen}
-                updateFitnessLog={updateFitnessLog}
-                deleteFitnessLog={deleteFitnessLog}
-                existingLog={editLog}
-            />
-            <h1 style={{textAlign: 'center'}}>Personal Fitness Tracker</h1> 
-            <div className="logging-container">
-                <FitnessGroups
-                    fitnessLogs={fitnessLogs}
-                    updateEditLog={updateEditLog} 
-                />
-            </div>
-            <div className="log-btn-row">
-                <Fab
-                    color="primary" 
-                    aria-label="add"
-                    onClick={() => {setEditLog(null); setAddOpen(true)}}
-                    className="new-log-button"
-                >
-                    <AddIcon />
-                </Fab>
-            </div>
-        </div>
+                    </thead>
+                    <tbody>
+                        {userRecs.map((doc: TableRow) => (
+                            <tr>
+                                <td>{doc.username}</td>
+                                <td>{doc.school}</td>
+                                <td>{doc.gyms}</td>
+                                <td>{doc.exercises.join(', ')}</td>
+                                <td>
+                                    <Button
+                                        variant="outlined"
+                                        sx={{width: '140px'}}
+                                        onClick={() => {alert('Profile viewer not implemented')}}
+                                    >
+                                        View Profile
+                                    </Button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </Row>
+            <Row>
+                <Button 
+                    variant="contained" 
+                    sx={{width: '100%', margin: '1rem auto 0', maxWidth: '200px'}}
+                    onClick={() => {navigate("/mainmenu", {state: username})}}
+                    >
+                        Go Back
+                </Button>
+            </Row>
+        </Container>
     )
 }
-*/
