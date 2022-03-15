fe521006dac626f8b4c4f5f68ee55e2fc362a1fc
Graeme Woods <graemewoods202@gmail.com>
2022-03-15 00:34:08 -0400

    add hours to mappings

--- src/recommendations/field_mappings.tsx
+++ src/recommendations/field_mappings.tsx
@@ -1,6 +1,7 @@
 export const GENDERS: { [key: string]: string } = {
   f: "Female",
   m: "Male",
+  n: "Non binary"
 };
 
 export const SCHOOLS: { [key: string]: string } = {
@@ -35,4 +36,31 @@ export const DAYS: { [key: string]: string } = {
   fri: "Friday",
   sat: "Saturday",
   sun: "Sunday",
+};
+
+export const HOURS:  { [key: number]: string } = {
+    0: '12 a.m.',
+    1: '1 a.m.',
+    2: '2 a.m.',
+    3: '3 a.m.',
+    4: '4 a.m.',
+    5: '5 a.m.',
+    6: '6 a.m.',
+    7: '7 a.m.',
+    8: '8 a.m.',
+    9: '9 a.m.',
+    10: '10 a.m.',
+    11: '11 a.m.',
+    12: '12 p.m.',
+    13: '1 p.m.',
+    14: '2 p.m.',
+    15: '3 p.m.',
+    16: '4 p.m.',
+    17: '5 p.m.',
+    18: '6 p.m.',
+    19: '7 p.m.',
+    20: '8 p.m.',
+    21: '9 p.m.',
+    22: '10 p.m.',
+    23: '11 p.m.',
 };
